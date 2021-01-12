abc5a3e03dafa0546ac94212e213d4c5f1c3285b
Richard Ascanio C <richardascanio@correo.unimet.edu.ve>
2021-01-12 14:30:14 -0400

    changes in instruction fragments

--- app/src/main/java/com/richard/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/richard/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.richard.foody.R
+import com.richard.foody.models.Result
+import com.richard.foody.utils.Constants.RECIPE_RESULT_KEY
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -18,7 +22,16 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        view.instructions_webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        view.instructions_webView.loadUrl(websiteUrl)
+
+        return view
     }
 
 }
